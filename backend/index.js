fef0e8f99bf69d9672db38873378d110f4909c3c
maalloor <maalloor@espol.edu.ec>
2022-02-13 17:07:23 -0500

    Project start! Add initial configuration for client, server, and database in Mongo Atlas

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// --> Mongo Atlas: https://www.mongodb.com/cloud/atlas/
+const CONNECTION_URL = 'mongodb+srv://maalloor:N6GRnu8hOkRagv0z@cluster0.vlrff.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Servidor ejecutándose en el puerto: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);
