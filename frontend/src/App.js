6ee012d84cf2457cbfb69ea3048e342c196b6dad
maalloor <maalloor@espol.edu.ec>
2022-02-14 23:11:27 -0500

    Definition of the JSX structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import experiences from './images/experiences.png';
+
+import Posts from './components/Posts/posts.js';
+import Form from './components/Form/form.js';
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Experiencias</Typography>
+                <img src={experiences} alt="experiences" height="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
---
+++ frontend/src/components/Posts/posts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Post from './Post/post.js';
+
+const Posts = () => {
+    return(
+        <>
+            <h1>POSTS</h1>
+            <Post />
+            <Post />
+        </>
+    );
+}
+
+export default Posts;
