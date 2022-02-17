5f5d316bd2adad89b017a472bdf03535856e380c
maalloor <maalloor@espol.edu.ec>
2022-02-16 23:12:24 -0500

    Applying basic styles for the form and publications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,18 @@ import experiences from './images/experiences.png';
 
 import Posts from './components/Posts/posts.js';
 import Form from './components/Form/form.js';
+import useStyles from './styles.js';
 
 const App = () => {
+
+    const classes = useStyles();
+
+
     return (
         <Container maxidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Experiencias</Typography>
-                <img src={experiences} alt="experiences" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Experiencias</Typography>
+                <img className={classes.image} src={experiences} alt="experiences" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
